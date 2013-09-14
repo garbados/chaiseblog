2bd65f4de699db6266d6239c5b7267becf9b0511
Max Thayer <garbados@gmail.com>
2013-09-14 19:45:21 -0400

    explicit routing

--- admin/index.js
+++ admin/index.js
@@ -2,14 +2,32 @@ var couchapp = require('couchapp')
   , path = require('path')
 
 ddoc = {
-    _id: '_design/chaiseblog'
-  , rewrites: require('../rewrites.json')
-  , views: couchapp.loadFiles(path.join(__dirname, '..', 'views'))
-  , filters: {
+    _id: '_design/chaise-admin',
+    rewrites: [{
+        "from": "",
+        "to": "index.html",
+        "method": "GET",
+        "query": {}
+    },{
+        "from": "/chaise-admin",
+        "to": "/../../",
+        "query": {} 
+    },{
+        "from": "/chaise-admin/*",
+        "to": "/../../*",
+        "query": {} 
+    },{
+        "from": "/*",
+        "to": "/*",
+        "method": "GET",
+        "query": {}
+    }],
+    views: couchapp.loadFiles(path.join(__dirname, '..', 'views')),
+    filters: {
       published: function(doc, req){ return doc.status }
-    }
-  , lists: {}
-  , shows: {}
+    },
+    lists: {},
+    shows: {}
 }
 
 couchapp.loadAttachments(ddoc, path.join(__dirname, '_attachments'))
