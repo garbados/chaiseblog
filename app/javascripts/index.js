d911c0b2176552185b56d7b4a350e0ba6f9995d6
Diana Thayer <me@dianathayer.org>
2015-07-09 11:41:58 -0700

    first draft

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -1,16 +1,13 @@
-var behave = require('./behave');
-var angular = require('angular');
-var markdown = require('marked');
-
 // initialize the app :D
-var app = angular.module('app', [
+require('angular').module('app', [
   // dependencies!
-  require('angular-route')
+  require('angular-route'),
+  require('angular-pouchdb')
 ])
 // url router
 .config([
-  '$routeProvider', '$locationProvider', 
-  function ($routeProvider, $locationProvider){
+  '$routeProvider', '$locationProvider',
+  function ($routeProvider, $locationProvider) {
     // routing convenience :D
     function route (template, ctrl, path) {
       $routeProvider.when(path, {
@@ -30,136 +27,107 @@ var app = angular.module('app', [
     $routeProvider.otherwise({ redirectTo: '/' });
   }
 ])
-// inject node dependencies as constants
-.constant('md', markdown)
-.constant('behave', behave)
-// root URL for the database. 
-.factory('root', ['$location', function ($location) {
-  if ($location.absUrl().indexOf('_rewrite') !== -1)
-    return '_rewrite/data';
-  else
-    return 'data';
-}])
+/* CONSTANTS */
+.constant('md', require('marked'))
+.constant('db', 'posts')
+.constant('ddoc', {
+  _id: '_design/diary',
+  views: {
+    all: {
+      map: function (doc) {
+        emit(doc.date, null);
+      }
+    },
+    drafts: {
+      map: function (doc) {
+        if (!doc.status)
+          emit(doc.date, null);
+      }
+    },
+    published: {
+      map: function (doc) {
+        if (doc.status)
+          emit(doc.date, null);
+      }
+    }
+  }
+})
+/* CREATE DDOC */
+.run([
+  'pouchDB', 'db', 'ddoc',
+  function (pouchDB, db, ddoc) {
+    pouchDB(db).put(ddoc);
+  }
+])
 // get posts from a given view
 .factory('getPosts', [
-  '$http', 'root', 
-  function ($http, root){
-    return function (viewname, opts){
-      var posts = $http({
-        url: [root, "_design/diary/_view", viewname].join('/'),
-        method: 'GET',
-        params: {
-          include_docs: true
-        }
+  'pouchDB', 'db',
+  function (pouchDB, db) {
+    return function (viewname, opts) {
+      var view = ['diary', viewname || 'all'].join('/');
+      return pouchDB(db).query(view, {
+        include_docs: true
       });
-      if(opts.success){
-        posts.success(function(data, status){
-          var docs = data.rows.map(function(row){
-            return row.doc;
-          });
-          opts.success(docs);
-        });
-      }
-      posts.error(function(){
-        if (opts.error) {
-          opts.error.apply(opts, arguments);
-        } else {
-          console.log(arguments);
-        }
-      });
-      return posts;
     };
   }
 ])
 // delete posts :O
 .factory('deletePost', [
-  '$http', 'root', 
-  function ($http, root){
-    return function (post){
-      return $http({
-        url: [root, post._id].join('/'),
-        method: 'DELETE',
-        params: {
-          rev: post._rev
-        }
-      });
-    };
-  }
-])
-// autosave posts while writing them
-.factory('autosave', [
-  '$http', '$timeout', 'root', 
-  function ($http, $timeout, root) {
-    return function (post){
-      (function _autosave(){
-        $http({
-          url: [root, post._id].join('/'),
-          method: 'PUT',
-          data: post
-        })
-        .success(function(data, status){
-          post._rev = data.rev;
-          // save every thirty seconds
-          $timeout(_autosave, 1000 * 30);
-        })
-        .error(function(){
-          console.log(arguments);
-        })
-        ;
-      })();
+  'pouchDB', 'db',
+  function (pouchDB, db) {
+    return function (post) {
+      return pouchDB(db).remove(post);
     };
   }
 ])
 // used to create controllers that list posts
 // such as PostsCtrl and DraftsCtrl
 .factory('listCtrl', [
-  'getPosts', 'deletePost', 
+  'getPosts', 'deletePost',
   function (getPosts, deletePost) {
-    return function ($scope, view){
-      $scope.delete = function(post){
-        if(confirm("Are you sure you want to delete that post?")){
-          deletePost(post).success(function(){
+    return function ($scope, view) {
+      $scope.delete = function (post) {
+        if(confirm("Are you sure you want to delete that post?")) {
+          deletePost(post).then(function () {
             $scope.posts = $scope.posts.filter(function (doc) {
               return doc._id !== post._id;
             });
           });
         }
       };
-      getPosts(view, {
-        success: function (docs) {
-          $scope.posts = docs;
-        }
+      getPosts(view).then(function (docs) {
+        $scope.posts = docs;
       });
     };
   }
 ])
 // list all posts
 .controller('PostsCtrl',  [
-  '$scope', 'listCtrl', 
-  function ($scope, listCtrl) { 
-    listCtrl($scope, 'published'); 
+  '$scope', 'listCtrl',
+  function ($scope, listCtrl) {
+    listCtrl($scope, 'published');
   }
 ])
 // list all drafts
 .controller('DraftsCtrl', [
-  '$scope', 'listCtrl', 
-  function ($scope, listCtrl) { 
+  '$scope', 'listCtrl',
+  function ($scope, listCtrl) {
     listCtrl($scope, 'drafts');
   }
 ])
 // form for a new post
 .controller('NewCtrl', [
-  '$scope', '$http', '$location', 'root', 
-  function ($scope, $http, $location, root){
-    $scope.submit = function(){
+  '$scope', '$http', '$location', 'root',
+  function ($scope, $http, $location, root) {
+    $scope.submit = function () {
       $scope.post.date = Date.now();
       $http({
         url: root,
         method: 'POST',
         data: $scope.post
-      }).success(function(data, status){
+      }).success(function (data, status) {
         $location.path('/');
-      }).error(function(){
+      }).error(function () {
         console.log(arguments);
       });
     };
@@ -167,20 +135,21 @@ var app = angular.module('app', [
 ])
 // form to edit existing post
 .controller('EditCtrl', [
-  '$scope', '$http', '$location', '$routeParams', 'root', 'autosave', 
-  function ($scope, $http, $location, $routeParams, root, autosave) {
-    var doc_url = [root, $routeParams.id].join('/'),
-        post = $http.get(doc_url);
-    post.success(function(data, status){
-      $scope.post = data;
-      autosave($scope.post);
+  '$scope', '$location', '$routeParams', 'pouchDB', 'db',
+  function ($scope, $location, $routeParams, pouchDB, db_name) {
+    var db = pouchDB(db_name);
+    var post = db.get($routeParams.id);
+    post.then(function (res) {
+      $scope.post = res;
     });
-    $scope.submit = function(){
-      $http.put(doc_url, $scope.post)
-      .success(function(data, status){
+
+    $scope.submit = function () {
+      db.put($scope.post)
+      .then(function () {
         $location.path('/');
       })
-      .error(function(){
+      .catch(function () {
+        // TODO error form
         console.log(arguments);
       });
     };
@@ -188,13 +157,10 @@ var app = angular.module('app', [
 ])
 // list a single post, draft or otherwise
 .controller('PostCtrl', [
-  '$scope', '$http', '$routeParams', 'root', 
-  function ($scope, $http, $routeParams, root){
-    var doc_url = [root, $routeParams.id].join('/'),
-        post = $http.get(doc_url);
-
-    post.success(function (data, status) {
-      $scope.posts = [data];
+  '$scope', '$routeParams', 'pouchDB', 'db',
+  function ($scope, $routeParams, pouchDB, db_name) {
+    pouchDB(db).get($routeParams.id).then(function (res) {
+      $scope.posts = [res];
     });
   }
 ])
@@ -208,22 +174,6 @@ var app = angular.module('app', [
       var html = md(input);
       var safe_html = $sce.trustAsHtml(html);
       return safe_html;
-    };
-  }
-])
-// a fancy-pantsy post-writing area
-.directive('behave', [
-  'behave',
-  function (behave) {
-    return {
-      restrict: 'A', // only activate on element attribute
-      link: function (scope, elem, attr) {
-        var textarea = elem[0];
-        var editor = new behave({ 
-          textarea: textarea,
-          fence: '```'
-        });
-      }
     };
   }
 ]);
