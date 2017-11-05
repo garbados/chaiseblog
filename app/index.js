116f3b112d7436bef2744d0365d16dd438142ae9
Diana Thayer <garbados@gmail.com>
2017-11-04 19:58:05 -0700

    experiment with partial separation

--- app/index.js
+++ app/index.js
@@ -9,6 +9,8 @@ import { h, render, Component } from 'preact'
 
 import { name } from '../package.json'
 
+import Nav from './nav'
+
 /*
 LOG
  */
@@ -296,23 +298,6 @@ class Home extends Component {
         <Entries view='entries' />
       )
     }
-  }
-}
-
-class Nav extends Component {
-  render () {
-    return (
-      <aside class='menu'>
-        <p class='menu-label'>
-          { name }
-        </p>
-        <ul class='menu-list'>
-          <li><a href='#/'>Home</a></li>
-          <li><a href='#/new-entry'>New Entry</a></li>
-          <li><a href='#/deleted'>Discarded Entries</a></li>
-        </ul>
-      </aside>
-    )
   }
 }
 
---
+++ app/nav.js
@@ -0,0 +1,18 @@
+import { h, render, Component } from 'preact'
+
+export default class Nav extends Component {
+  render () {
+    return (
+      <aside class='menu'>
+        <p class='menu-label'>
+          { name }
+        </p>
+        <ul class='menu-list'>
+          <li><a href='#/'>Home</a></li>
+          <li><a href='#/new-entry'>New Entry</a></li>
+          <li><a href='#/deleted'>Discarded Entries</a></li>
+        </ul>
+      </aside>
+    )
+  }
+}
